5a7d5e3e9b90a53bd25e8da5893576705f254365
Christof Röhl <userpattern@gmail.com>
2024-01-24 13:08:09 +0100

    solved + liilte extra test

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ console.clear();
 const form = document.querySelector("form");
 const output = form.querySelector("output");
 
+const formDivide = document.querySelector("[data-js=form2]");
+
+const errorDisplay = document.querySelector("[data-js=error-display]");
+const errorDisplay2 = document.querySelector("[data-js=error-display2]");
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
@@ -20,5 +24,24 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number(event.target.firstNumber.value);
   const secondNumber = Number(event.target.secondNumber.value);
   const operation = event.target.operation.value;
-  output.innerText = operations[operation](firstNumber, secondNumber);
+  try {
+    output.innerText = operations[operation](firstNumber, secondNumber);
+  } catch (error) {
+    errorDisplay.textContent = "You shall not divide by zero! Please don't!";
+  }
+});
+
+function divideOnly(a, b) {
+  if (b === 0) {
+    throw new Error("Though shall not divide by zero!");
+  }
+  return a / b;
+}
+const divisionResult = document.querySelector("[data-js=division-result]");
+formDivide.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.dividend.value);
+  const secondNumber = Number(event.target.divisor.value);
+  const quotient = divideOnly(firstNumber, secondNumber);
+  divisionResult.textContent = quotient;
 });
